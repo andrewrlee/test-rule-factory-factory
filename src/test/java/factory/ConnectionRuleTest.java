62e74f0fdefb50a39aab4cda3971760ceb9044ef
plasma147 <plasma147@hotmail.com>
2015-05-23 09:49:13 +0100

    Now there are 4 types of methods we can call, each one exposes how we create the connection. This is fine for a few tests but when the number of tests reach large numbers, if we want to change how  the connections are built then we are going to have to change a lot of tests

--- src/test/java/factory/ConnectionRuleTest.java
+++ src/test/java/factory/ConnectionRuleTest.java
@@ -13,13 +13,20 @@ import factory.Connection.ConnectionB;
 public class ConnectionRuleTest {
 
 	@Rule
-	public ConnectionRule<ConnectionA> ruleA = ConnectionRule.create(ConnectionA::new);
+	public ConnectionRule<ConnectionA> ruleA1 = ConnectionRule.create(ConnectionA::new);
 	@Rule
-	public ConnectionRule<ConnectionB> ruleB = ConnectionRule.create(ConnectionB::new, Collections.singleton("http://localhost"));
+	public ConnectionRule<ConnectionA> ruleA2 = ConnectionRule.create(ConnectionA::new, Collections.singleton("http://localhost"));
+	@Rule
+	public ConnectionRule<ConnectionB> ruleB1 = ConnectionRule.create(ConnectionB::new);
+	@Rule
+	public ConnectionRule<ConnectionB> ruleB2 = ConnectionRule.create(ConnectionB::new, Collections.singleton("http://localhost"));
 	
 	@Test
 	public void checkConnectionNotNull(){
-		assertThat(ruleA.getConnection()).isNotNull();
-	}
+		assertThat(ruleA1.getConnection()).isNotNull();
+		assertThat(ruleA2.getConnection()).isNotNull();
+		assertThat(ruleB1.getConnection()).isNotNull();
+		assertThat(ruleB2.getConnection()).isNotNull();
+	}	
 	
 }
